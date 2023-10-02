3007a2b8e474e10010703c830ea4bac66b063ccf
xRezio <edwin.carin@epitech.eu>
2023-10-02 14:57:06 +0200

    Update task04.js

--- task04.js
+++ task04.js
@@ -2,10 +2,6 @@
 
 // Function to implement FizzBuzz
 export function fizzBuzz(number) {
-    if (typeof number !== 'number' || isNaN(number) || number <= 0) {
-      console.log("Invalid input. Please provide a positive number.");
-      return;
-    }
   
     let result = '';
     for (let i = 1; i <= number; i++) {
